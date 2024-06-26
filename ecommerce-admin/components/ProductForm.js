ced97d78c101ad2c9413511a6b1f7ca6c95fea51
Angel Francisco <ang3lfco@outlook.com>
2024-06-26 04:20:50 -0700

    point - 03:33:00

--- ecommerce-admin/components/ProductForm.js
+++ ecommerce-admin/components/ProductForm.js
@@ -2,18 +2,22 @@ import { data } from "autoprefixer";
 import axios from "axios";
 import { useRouter } from "next/router";
 import { useState } from "react";
+import Spinner from "./Spinner";
+import { ReactSortable } from "react-sortablejs";
 
-export default function ProductForm({ _id, title:existingTitle, description:existingDescription, price:existingPrice, images}){
+export default function ProductForm({ _id, title:existingTitle, description:existingDescription, price:existingPrice, images:existingImages}){
     const [title, setTitle] = useState(existingTitle || '');
     const [description, setDescription] = useState(existingDescription || '');
     const [price, setPrice] = useState(existingPrice || '');
     const [goToProducts, setGoToProducts] = useState(false);
+    const [images, setImages] = useState(existingImages || []);
     const router = useRouter();
+    const [isUploading, setIsUploading] = useState(false);
     
-    //fetch o axios
-    async function createProduct(ev){
+    //fetch or axios
+    async function saveProduct(ev){
         ev.preventDefault();
-        const data = {title, description, price};
+        const data = {title, description, price, images};
         if(_id){
             await axios.put('/api/products', {...data, _id}); //update
         }
@@ -25,6 +29,7 @@ export default function ProductForm({ _id, title:existingTitle, description:exis
     if(goToProducts){
         router.push('/Products');
     }
+    
     async function uploadImages(ev){
         console.log(ev);
         const files = ev.target?.files;
@@ -32,23 +37,45 @@ export default function ProductForm({ _id, title:existingTitle, description:exis
             /* en ves de un json como de costumbre guardamos todo en un FormData
             sera mas facil el parse en el lado del back end
             */
+           setIsUploading(true);
             const data = new FormData();
             for(const file of files){
                 data.append('file', file);
             }
-            const res = await axios.post('/api/upload', data, {
-                headers: {'Content-Type': 'multipart/form-data'},
+            const res = await axios.post('/api/upload', data);
+            setImages(oldImages =>{
+                return [...oldImages, ...res.data.links];
             });
+            setIsUploading(false);
             console.log(res.data);
         }
     }
+
+    function updateImagesOrder(images){
+        setImages(images);
+        console.log(images);
+    }
+
     return(
-        <form onSubmit={createProduct}>
+        <form onSubmit={saveProduct}>
             <h1>New Product</h1>
             <label>Product Name</label>
             <input type="text" placeholder="product name" value={title} onChange={ev => setTitle(ev.target.value)}></input>
             <label>Photos</label>
-            <div className="mb-2">
+            <div className="mb-2 flex flex-wrap gap-1">
+                <ReactSortable list={images} className="flex flex-wrap gap-1" setList={updateImagesOrder}>
+                    {!!images?.length && images.map(link => (
+                        <div key={link} className="h-24">
+                            {/* <div>{link}</div> */}
+                            <img src={link} alt="" className="rounded-lg w-full h-full"/>
+                        </div>
+                    ))}
+                </ReactSortable>
+                {isUploading && (
+                    <div className="h-24 flex items-center">
+                        <Spinner/>
+                    </div>
+                )}
                 <label className="w-24 h-24 cursor-pointer text-center flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" 
@@ -56,7 +83,8 @@ export default function ProductForm({ _id, title:existingTitle, description:exis
                     </svg>
                     <div>Upload</div>
                     <input type="file" onChange={uploadImages} className="hidden"></input>
-                </label>{!images?.lenght && (<div>No photos in this product</div>)}
+                </label>
+                {/* {!images?.length && (<div>No photos in this product</div>)} */}
             </div>
             <label>Description</label>
             <textarea placeholder="description" value={description} onChange={ev => setDescription(ev.target.value)}></textarea>
--- ecommerce-admin/pages/api/upload.js
+++ ecommerce-admin/pages/api/upload.js
@@ -1,5 +1,10 @@
 import multiparty from 'multiparty';
 import { resolve } from 'styled-jsx/css';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import fs from 'fs';
+import mime from 'mime-types';
+
+const bucketName = 'ang3lfco-next-ecommerce';
 
 export default async function handle(req, res){
     const form = new multiparty.Form();
@@ -10,9 +15,32 @@ export default async function handle(req, res){
             
         });
     });
-    console.log('length:', files.length);
+    console.log('length:', files.file.length);
     console.log(fields);
-    res.json('ok');
+
+    const client = new S3Client({
+        region: 'us-east-2',
+        credentials: {
+            accessKeyId: process.env.S3_ACCESS_KEY,
+            secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+        },
+    });
+    const links = [];
+    for(const file of files.file){
+        const ext = file.originalFilename.split('.').pop();
+        const newFilename = Date.now() + '.' + ext;
+        console.log({ext, file});
+        await client.send(new PutObjectCommand({
+            Bucket: bucketName,
+            Key: newFilename,
+            Body: fs.readFileSync(file.path),
+            ACL: 'public-read',
+            ContentType: mime.lookup(file.path),
+        }));
+        const link = `https://${bucketName}.s3.amazonaws.com/${newFilename}`;
+        links.push(link);
+    }
+    return res.json({links});
 }
 
 export const config = {
