609ca72befb8a004a6d697ec7a3d9005e6949f6b
ang3lfco <ang3lfco@outlook.com>
2024-08-08 00:47:02 -0700

    11:17:54 - single product page done

--- ecommerce-front/components/Header.js
+++ ecommerce-front/components/Header.js
@@ -38,10 +38,10 @@ export default function Header(){
                     <Logo href={'/'}>Ecommerce</Logo>
                     <StyledNav>
                         <NavLink href={''}>Home</NavLink>
-                        <NavLink href={'products'}>All products</NavLink>
+                        <NavLink href={'/products'}>All products</NavLink>
                         <NavLink href={'categories'}>Categories</NavLink>
                         <NavLink href={'account'}>Account</NavLink>
-                        <NavLink href={'cart'}>Cart ({cartProducts.length})</NavLink>
+                        <NavLink href={'/cart'}>Cart ({cartProducts.length})</NavLink>
                     </StyledNav>
                 </Wrapper>
             </Center>
