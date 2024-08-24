045d6cb0fd3e4906a66a2239147dcc977ecfbe68
ang3lfco <ang3lfco@outlook.com>
2024-08-23 18:48:51 -0700

    solved

--- ecommerce-admin/pages/Products/delete/[...id].js
+++ ecommerce-admin/pages/Products/delete/[...id].js
@@ -27,7 +27,7 @@ export default function DeleteProductPage(){
 
     return(
         <Layout>
-            <h1 className="text-center">Do you really want to delete &nbsp;"{productInfo?.title}"?</h1>
+            <h1 className="text-center">Do you really want to delete &quot;{productInfo?.title}&quot;?</h1>
             <div className="flex gap-2 justify-center">
                 <button onClick={deleteProduct} className="btn-red">Yes</button>
                 <button className="btn-default" onClick={goBack}>No</button>
