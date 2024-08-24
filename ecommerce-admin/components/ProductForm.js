8fe4b94d732939215b1a8aeaac88361512d25f34
ang3lfco <ang3lfco@outlook.com>
2024-08-23 19:01:25 -0700

    solved

--- ecommerce-admin/components/ProductForm.js
+++ ecommerce-admin/components/ProductForm.js
@@ -94,16 +94,16 @@ export default function ProductForm({ _id, title:existingTitle, description:exis
             <select value={category} onChange={ev => setCategory(ev.target.value)}>
                 <option value="">Uncategorized</option>
                 {categories.length > 0 && categories.map(c => (
-                    <option value={c._id}>{c.name}</option>
+                    <option key={c._id} value={c._id}>{c.name}</option>
                 ))}
             </select>
             {propertiesToFill.length > 0 && propertiesToFill.map(p => (
-                <div className="">
+                <div key={p.name} className="">
                     <label>{p.name[0].toUpperCase()+p.name.substring(1)}</label>
                     <div>
                         <select value={productProperties[p.name]} onChange={ev => setProductProp(p.name, ev.target.value)}>
                             {p.values.map(v => (
-                                <option value={v}>{v}</option>
+                                <option key={v} value={v}>{v}</option>
                             ))}
                         </select>
                     </div>
